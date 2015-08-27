15dbbc75e8ca1161a900a7b442881af93d6c36eb
Alexandre Demode <contact@alex-d.fr>
2015-08-27 12:13:03 +0200

    fix(scss): remove cache because is bugged and useless

--- example-projects/simple-html-css-js/gulpfile.js
+++ example-projects/simple-html-css-js/gulpfile.js
@@ -5,29 +5,33 @@ var one = require('one-gulp'),
 
 one.init(gulp, {
 
-    cssDeps: [
-        {
-            include: ['bower_components/**/*.css'],
-            output: 'bower-styles.css'
-        },
-        {
-            include: ['zzz.css', 'skin/*.css', '**/*.css'],
-            exclude: ['bower_components/**/*.css', 'ddd.css'],
-            output: 'all-styles.css'
-        }
-    ],
+    css: {
+        deps: [
+            {
+                include: ['bower_components/**/*.css'],
+                output: 'bower-styles.css'
+            },
+            {
+                include: ['zzz.css', 'skin/*.css', '**/*.css'],
+                exclude: ['bower_components/**/*.css', 'ddd.css'],
+                output: 'all-styles.css'
+            }
+        ]
+    },
 
-    jsDeps: [
-        {
-            include: ['bower_components/**/*.js'],
-            output: 'bower-scripts.js'
-        },
-        {
-            include: ['gallery/*.js', 'index.js', '**/*.js'],
-            exclude: ['bower_components/**/*.js'],
-            output: 'all-scripts.js'
-        }
-    ],
+    javascript: {
+        deps: [
+            {
+                include: ['bower_components/**/*.js'],
+                output: 'bower-scripts.js'
+            },
+            {
+                include: ['gallery/*.js', 'index.js', '**/*.js'],
+                exclude: ['bower_components/**/*.js'],
+                output: 'all-scripts.js'
+            }
+        ]
+    },
 
     watchPaths: [
         'bower_components'
